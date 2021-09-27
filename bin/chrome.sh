cbb24c7475cf7fb57593e1c433245ea7b3bf070f
m47h4r <m47h4r@gmail.com>
2021-09-27 21:58:07 +0330

    cleanup, remove i3 config, I'm too old for this shit

--- bin/chrome.sh
+++
@@ -1,3 +0,0 @@
-google-chrome-stable & disown
-#google-chrome-stable --proxy-server="http://localhost:8888" & disown
-#google-chrome-stable --proxy-server="socks5://localhost:9050" & disown
--- polybar/launch.sh
+++
@@ -1,7 +0,0 @@
-killall -q polybar
-
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-	MONITOR=$m polybar top_bar &
-done
-
-echo "Launched the fuck out of the bars!!!"
